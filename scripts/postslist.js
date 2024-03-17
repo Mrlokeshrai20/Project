21b21258bf4a94fa5d4bae7da69cd858e3e7e353
shivakumar <shivakumar.yevunja@gmail.com>
2024-03-17 13:27:54 +0530

    the project is completed

--- scripts/postslist.js
+++ scripts/postslist.js
@@ -1,20 +1,47 @@
+var idval = '';
+var overlay = document.getElementById('overlay');
+var delContent = document.getElementById('delete-content');
 const delete_btn = (id)=>{
     this.addEventListener('click',()=>{
-        if(id == 'delete-1'){
-            const element = document.getElementById("card-1");
+            overlay.classList.add('active');
+            delContent.classList.add('active');
+            idval = id;
+            console.log(idval);
+    })
+}
+
+const yesBtn = ()=>{
+    this.addEventListener('click',()=>{
+        overlay.classList.remove('active');
+        delContent.classList.remove('active');
+        if(idval == 'delete-1'){
+            var cardid = 'card-1';
+            const element = document.getElementById(cardid);
             element.remove();
-        }else if(id == 'delete-2'){
-            const element = document.getElementById("card-2");
+        }else if(idval == 'delete-2'){
+            var cardid = 'card-2';
+            const element = document.getElementById(cardid);
             element.remove();
-        }else if(id == 'delete-3'){
-            const element = document.getElementById("card-3");
+        }else if(idval == 'delete-3'){
+            var cardid = 'card-3';
+            const element = document.getElementById(cardid);
             element.remove();
-        }else if(id == 'delete-4'){
-            const element = document.getElementById("card-4");
+        }else if(idval == 'delete-4'){
+            var cardid = 'card-4';
+            const element = document.getElementById(cardid);
             element.remove();
-        }else if(id == 'delete-5'){
-            const element = document.getElementById("card-5");
+        }else if(idval == 'delete-5'){
+            var cardid = 'card-5';
+            const element = document.getElementById(cardid);
             element.remove();
         }
     })
+}
+
+const noBtn = ()=>{
+    this.addEventListener('click',()=>{
+        overlay.classList.remove('active');
+        delContent.classList.remove('active');
+    })
+    
 }
